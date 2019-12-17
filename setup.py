751c9e48fceee534ed9469087dbeb0a1dd990d73
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2019-12-17 20:10:10 -0300

    Build package for PyPI distribution

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Leonardo Gregianin, Gustavo Efeiche",
+    author_email="leogregianin@gmail.com",
+    name='ibge',
+    license="MIT",
+    description='Collection of APIs for the IBGE Data Services in Brazil. (https://servicodados.ibge.gov.br/api/docs)',
+    version='0.0.1',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/leogregianin/ibge/',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Portuguese (Brazilian)'
+    ]
+)
